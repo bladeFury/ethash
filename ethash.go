c74b06d9047ab053eab04e4358a0242a60a9ef13
zhangge <uniqguang@gmail.com>
2018-03-30 13:25:27 +0800

    make verify return target

--- ethash.go
+++ ethash.go
@@ -125,13 +125,13 @@ type Light struct {
 }
 
 // Verify checks whether the block's nonce is valid.
-func (l *Light) Verify(block Block) (bool, common.Hash) {
+func (l *Light) Verify(block Block) (bool, common.Hash, *big.Int) {
 	// TODO: do ethash_quick_verify before getCache in order
 	// to prevent DOS attacks.
 	blockNum := block.NumberU64()
 	if blockNum >= epochLength*2048 {
 		log.Debug(fmt.Sprintf("block number %d too high, limit is %d", epochLength*2048))
-		return false, common.Hash{}
+		return false, common.Hash{}, nil
 	}
 
 	difficulty := block.Difficulty()
@@ -142,7 +142,7 @@ func (l *Light) Verify(block Block) (bool, common.Hash) {
 	*/
 	if difficulty.Cmp(common.Big0) == 0 {
 		log.Debug("invalid block difficulty")
-		return false, common.Hash{}
+		return false, common.Hash{}, nil
 	}
 
 	cache := l.getCache(blockNum)
@@ -152,13 +152,8 @@ func (l *Light) Verify(block Block) (bool, common.Hash) {
 	}
 	// Recompute the hash using the cache.
 	ok, mixDigest, result := cache.compute(uint64(dagSize), block.HashNoNonce(), block.Nonce())
-	if !ok {
-		return false, mixDigest
-	}
 
-	// The actual check.
-	target := new(big.Int).Div(maxUint256, difficulty)
-	return result.Big().Cmp(target) <= 0, mixDigest
+	return ok, mixDigest, result.Big()
 }
 
 func h256ToHash(in C.ethash_h256_t) common.Hash {
